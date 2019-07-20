9715959cde16ad526b00c59d30897c8d848b8cbf
sigh_kgr <tm.hasegawa98@gmail.com>
2019-07-20 21:02:14 +0900

    [AC] abc134 abc134_a

---
+++ atcoder.jp/abc134/abc134_a/Main.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+class Program{
+    static void Main(){
+        
+        int r = int.Parse(Console.ReadLine());
+        
+        r = 3*r*r;
+        
+        Console.WriteLine(r);
+    }
+}
