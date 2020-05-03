81f4afd4772a07f7b6da0ce9fc3b39b352ec3349
sigh_kgr <tm.hasegawa98@gmail.com>
2020-05-03 21:03:07 +0900

    [AC] abc166 abc166_a

---
+++ atcoder.jp/abc166/abc166_a/Main.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = Console.ReadLine()=="ABC" ? "ARC" : "ABC";
+            Console.WriteLine(s);
+
+        }
+    }
+}
